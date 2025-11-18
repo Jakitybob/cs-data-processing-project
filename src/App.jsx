367caf79399c9f6051a37098482f7fe8527d56e7
Jakitybob <jakitybob@gmail.com>
2025-11-17 23:35:30 -0600

    Added dataset format guide and then promptly broke it
    There is a small data set format guide but it is currently not showing up because I am toying around with swapping its display with the list of reports when that is ready, so tomorrow hopefully.

--- src/App.jsx
+++ src/App.jsx
@@ -26,19 +26,48 @@ export default function App() {
 
   return (
     <div>
-      <Header />
-      <body class="main-body">
-        <input type="file" accept=".txt" onChange={HandleFileChange} />
-      </body>
+      <header>
+        <h1>Intelligence Analysis</h1>
+        <div className="subheader">
+          Upload a dataset to analyze:
+          <input type="file" accept=".txt" onChange={HandleFileChange} /> 
+        </div>
+      </header>
+      <MainComponentDisplay array={array} />
     </div>
   )
 }
 
-// The persistent header for the application
-function Header() {
+// Displays the data set format guide if the array is empty, or the report viewer if it is not empty
+function MainComponentDisplay(array) {
+  console.log(array);
+  // Display the data set guide if the array is empty
+  if (array == undefined || array.length < 1) {
+    console.log("dataset!");
+    return (<DatasetFormat />);
+  } 
+
+  // Return the 
+}
+
+// Display the format guidelines for a dataset
+function DatasetFormat() {
   return (
-    <header>
-      <h1>Intelligence Analysis</h1>
-    </header>
-  );
+    <div class="info-box">
+      <h3>Dataset Format Guidelines</h3>
+      <p>
+        The dataset must follow these formatting guidelines to be properly analyzed:
+      </p>
+      <ul>
+        <li>Each report must be separated with a line that says "REPORT: "</li>
+        <li>
+          There must be lines started with ID:, REPORTDATE:, REFERENCEID:, REPORTSOURCE:,
+          REPORTDESCRIPTION:, PERSONS:, DATES:, and ORGANIZATIONS:.
+        </li>
+        <li>Dates must be in the format MM/DD/YYYY, however month and day can have one digit.</li>
+        <li>Places must be separated into four locations. If one is blank they still need a space. (e.g. Road/Town/State/Place)</li>
+        <li>Any lists must be separated by a semi-colon.</li>
+      </ul>
+    </div>
+  )
 }
